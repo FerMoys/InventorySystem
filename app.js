2a8e5858825fb81b75f1646bf02340d7166237ad
mafer <mafermm834@gmail.com>
2026-05-21 13:00:40 -0600

    correciones a app.js

--- app.js
+++ app.js
@@ -20,16 +20,6 @@ app.use('/auth', authRoutes);
 
 app.use('/products', productRoutes);
 
-require('dotenv').config();
-
-const express = require('express');
-
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors());
-
 app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');
